fbef9e11e8f2f1f6f5091afa17a7fdbe00103e2b
olgamartinchik <sintcerova@mail.ru>
2020-03-21 20:12:19 +0300

    footer

--- js/scripts.js
+++ js/scripts.js
@@ -126,3 +126,12 @@ $('.slide-hurry').slick({
     arrows: true,
     dots: false,
 });
+// $('.slide-blog').slick({
+
+//     // arrows: false,
+//     dots: false,
+//     slidesToShow: 3,
+//     // slidesToScroll: 1,
+//     // autoplay: true,
+//     // autoplaySpeed: 5000,
+// });

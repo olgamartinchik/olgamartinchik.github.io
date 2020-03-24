8887c60d7955d7cb0d4930239e040a3aefe965bd
olgamartinchik <sintcerova@mail.ru>
2020-03-24 23:15:09 +0300

    scroll-menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 'use strict';
 
+window.onscroll = function() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    document.getElementById('header-menu').style.backgroundColor = scrolled == 0 ? "rergba(0, 0, 0, 0.081)" : "rgba(0, 0, 0, 0.251)";
+}
+
 function Tabs() {
     var bindAll = function() {
         var menuElements = document.querySelectorAll('[data-tab]');
@@ -63,13 +68,13 @@ $(document).ready(function() {
     $('.slide-norway').slick({
         arrows: true,
         dots: true,
-        nextArrow: ' <i class="slick-next fa-chevron-right"></i>',
-        prevArrow: '<i class="slick-prev fa-chevron-left"></i>',
+        // nextArrow: ' <i class="slick-next fa-chevron-right"></i>',
+        // prevArrow: '<i class="slick-prev fa-chevron-left"></i>',
         loop: true,
         responsive: [{
                 breakpoint: 768,
                 settings: {
-                    arrows: false,
+                    arrows: true,
                     centerMode: true,
                     centerPadding: '40px',
                     slidesToShow: 1
@@ -78,7 +83,7 @@ $(document).ready(function() {
             {
                 breakpoint: 480,
                 settings: {
-                    arrows: false,
+                    arrows: true,
                     centerMode: true,
                     centerPadding: '40px',
                     slidesToShow: 1
@@ -105,27 +110,15 @@ $(document).ready(function() {
         arrows: true,
         dots: false,
     });
-
-    $('#countdown_dashboard').countdown({
-        targetOffset: {
-            'day': 360, //число
-            'month': 0, //месяц
-            'year': 0, //год           
-            'hour': 30, //часов
-            'min': 1, //минут
-            'sec': 1 //секунд
-        }
-    });
 });
 
-function start() {
-    $('#countdown_dashboard').startCountDown();
-}
+
 
 $('.slide-hurry').slick({
     arrows: true,
     dots: false,
 });
+
 // $('.slide-blog').slick({
 
 //     // arrows: false,
@@ -135,3 +128,47 @@ $('.slide-hurry').slick({
 //     // autoplay: true,
 //     // autoplaySpeed: 5000,
 // });
+
+// counter
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = "January 01 2030 00:00:00 GMT+0300";
+var deadline = new Date(Date.parse(new Date()) + 365 * 24 * 60 * 60 * 1000); // for endless timer
+initializeClock('countdown', deadline);
